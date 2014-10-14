6986a35cc179a290ea70fc052abd14fe4fc19a01
Casey Dahlin <casey.dahlin@gmail.com>
2014-10-14 15:34:59 -0400

    Use version of module from build folder to generate docs
    Signed-off-by: Casey Dahlin <casey.dahlin@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -11,12 +11,18 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import sysconfig
 import sys, os
+
+platform = sysconfig.get_platform()
+version = sys.version_info[:2]
+version = str(version[0]) + '.' + str(version[1])
+platform = platform + '-' + version
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../build/lib.' + platform))
 
 # -- General configuration -----------------------------------------------------
 
